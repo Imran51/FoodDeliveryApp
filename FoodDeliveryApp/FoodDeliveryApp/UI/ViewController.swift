9246a67340d36140a95754edc35aabefa252b32d
Imran51 <imransayeed.ruet12@gmail.com>
2021-05-09 11:26:51 +0600

    Init: Food delivery Project Created with Basic Folder Structure

---
+++ FoodDeliveryApp/FoodDeliveryApp/UI/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Imran Sayeed on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
